d43bcacdaaec29df2e2dc438c97534288830dbac
gsher9991 <gsher9991@gmail.com>
2022-06-29 13:57:50 +0500

    comments removal

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,11 @@ import "./App.css";
 import Flowers from "./components/Flowers";
 import FlowersData from "./components/FlowersData";
 
-// const FlowersInfo = (values) => {
-//   return (
-//     <>
-//       <Flowers
-//         title={values.title}
-//         imgsrc={values.imgsrc}
-//         desc={values.desc}
-//         btn={values.btn}
-//       />
-//     </>
-//   );
-// };
-
 function App() {
   return (
     <div>
       <h1>About Flowers</h1>
       <div className="container">
-        {/* <Flowers
-          title={FlowersData[0].title}
-          imgsrc={FlowersData[0].imgsrc}
-          desc={FlowersData[0].desc}
-          btn={FlowersData[0].btn}
-        /> */}
         {FlowersData.map((values) => {
           return (
             <>
